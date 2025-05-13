4987efb7622a190c10cd364532f3177c147f18c1
Tdev <161383528+GregoireBarral@users.noreply.github.com>
2025-05-13 13:03:45 +0200

    Update memoire.py

--- memoire.py
+++ memoire.py
@@ -221,8 +221,7 @@ tab1, tab2, tab3, tab4, tab5= st.tabs(["illiquidity over time", "portfolio optim
 with tab1: 
 
     
-    st.dataframe(values.head())
-    st.dataframe(volume.head())
+    st.dataframe(monthly_illiq.head())
    
     st.subheader("**Disclaimer**")
     st.write("""
